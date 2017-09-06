da897ccaaae999efb86de732bcee836a39860442
NAKATSUKA, Yukitaka <zool@zool.jpn.org>
2017-09-07 02:36:44 +0900

    Fix libandroid-shmem.rb

--- libandroid-shmem.rb
+++ libandroid-shmem.rb
@@ -3,8 +3,8 @@ eval (Tap.fetch("z80oolong/debian-noroot").formula_dir/"lib/install_preload_so.r
 class LibandroidShmem < Formula
   include InstallPreloadSO
 
-  desc "libandroid-shmem.so on Termux"
-  homepage "http://zool.jpn.org/"
+  desc "libandroid-shmem.so by Termux developer team on Debian noroot"
+  homepage "https://gist.github.com/z80oolong/247dbbb0a7d83a1dea98de2939327432"
   url "https://github.com/termux/libandroid-shmem/archive/v0.2.tar.gz"
   version "0.2"
   sha256 "75e687f48c01d96ea1345f11c064ff76f7eb9a119f51ebe2f6253cf8b24a2b97"
@@ -14,8 +14,8 @@ class LibandroidShmem < Formula
   end
 
   patch do
-    url "https://gist.githubusercontent.com/z80oolong/247dbbb0a7d83a1dea98de2939327432/raw/da08c875c5dba1d5fd42745031998aa54624fa24/libandroid-shmem-termux-0.2-fix.diff"
-    sha256 "ede3f814b09d3151a4702d0890f52898c1339aafb9db49f034af59bf811ae1b4"
+    url "https://gist.githubusercontent.com/z80oolong/247dbbb0a7d83a1dea98de2939327432/raw/36b2bd0bf29faf0c43dbcfbedde5d8b48a92aaf3/libandroid-shmem-termux-0.2_1-fix.diff"
+    sha256 "cf3409ce8ef642e72a00e51c80e6993db3d4fe2886d19dff013ae350055236fc"
   end
 
   def install
